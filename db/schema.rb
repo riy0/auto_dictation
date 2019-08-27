c658e528a530eccf5b03931d26c308337d858d5e
riy0 <chac0ptx@gmail.com>
2019-08-27 16:38:16 +0900

    done user process

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_052643) do
+ActiveRecord::Schema.define(version: 2019_08_27_073430) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_052643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
