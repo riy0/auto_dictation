6770e8dfb63713517d026bf220a8f8ed3d7feef6
riy0 <chac0ptx@gmail.com>
2019-08-27 15:30:12 +0900

    add session

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   resources :users
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
   get '/help' => 'static_pages#help'
